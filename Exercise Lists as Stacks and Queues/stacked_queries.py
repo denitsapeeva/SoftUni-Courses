956c4e217aa328d89a41e80993c3551adaeb9810
Denitsapeeva <denitsa.peeva@yahoo.com>
2023-01-12 14:52:26 +0200

    Studying programming

--- Exercise Lists as Stacks and Queues/stacked_queries.py
+++ Exercise Lists as Stacks and Queues/stacked_queries.py
@@ -1,17 +1,17 @@
 n = int(input())
 my_stack = []
 for i in range(n):
-    queries =input().split(" ")
+    queries = input().split(" ")
     command = queries[0]
     if command == '1':
         my_stack.append(int(queries[1]))
-    elif command == '2':
+    elif command == '2' and my_stack:
         my_stack.pop()
     elif command == '3':
         print(max(my_stack))
     elif command == '4':
         print(min(my_stack))
 str_stack = []
-for i in range(len(my_stack)):
+while my_stack:
     str_stack.append(str(my_stack.pop()))
 print(', '.join(str_stack))
