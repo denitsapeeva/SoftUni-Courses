c938193a555d0c57ae0e124c86c6bd2071a251d2
Denitsapeeva <denitsa.peeva@yahoo.com>
2023-01-14 10:58:20 +0200

    Studying programming

--- Exercise Lists as Stacks and Queues/balanced_parentheses.py
+++ Exercise Lists as Stacks and Queues/balanced_parentheses.py
@@ -1,35 +1,18 @@
-my_expression = input()
-right_expression = []
-left_expression = []
-stop_index = 0
-my_bool = True
-for i in range(len(my_expression)):
-    if my_expression[i] != ')':
-        right_expression.append(my_expression[i])
-    else:
-        stop_index = i
+from _collections import deque
+parentheses = deque(input())
+open_paranthesis = deque()
+while parentheses:
+    left_parantheses = parentheses.popleft()
+    if left_parantheses in "({[":
+        open_paranthesis.append(left_parantheses)
+    elif not open_paranthesis:
+        print("NO")
         break
-for j in range(stop_index,len(my_expression)):
-    left_expression.append(my_expression[j])
-
-if len(right_expression) == len(left_expression):
-    while right_expression:
-        right = right_expression.pop()
-        left = left_expression.pop(0)
-        if right == '{' and left == '}':
-            continue
-        elif right == '(' and left == ')':
-            continue
-        elif right == '[' and left == ']':
-            continue
-        else:
-            my_bool = False
+    else:
+        if f"{open_paranthesis.pop() + left_parantheses}" not in "{}[]()":
+            print("NO")
+            break
 else:
-    my_bool = False
-
-if my_bool:
     print("YES")
-else:
-    print("NO")
 
 
