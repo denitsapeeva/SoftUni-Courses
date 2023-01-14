3a56a653b736e06697fa75f9cf00c42983d745dc
Denitsapeeva <denitsa.peeva@yahoo.com>
2023-01-14 15:47:32 +0200

    Studying programming

---
+++ Tuples and Sets/softUni_party.py
@@ -0,0 +1,11 @@
+reservations = set([input() for _ in range(int(input()))])
+guests_at_the_party = set()
+while True:
+    command = input()
+    if command == "END":
+        break
+    guests_at_the_party.add(command)
+left_reservations = reservations.difference(guests_at_the_party)
+print(len(left_reservations))
+for guest in sorted(left_reservations):
+    print(guest)
