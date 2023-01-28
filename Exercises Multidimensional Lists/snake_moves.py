34de98bce0377c753b32a99a05225db05158ad36
Denitsapeeva <denitsa.peeva@yahoo.com>
2023-01-28 15:45:11 +0200

    Studying programming

---
+++ Exercises Multidimensional Lists/snake_moves.py
@@ -0,0 +1,13 @@
+from collections import deque
+row, col = (int(x) for x in input().split())
+snake = list(input())
+snake_copy = deque(snake)
+
+for r in range(row):
+    while len(snake_copy) < col:
+        snake_copy.extend(snake)
+
+    if r % 2 == 0:
+        print(*[snake_copy.popleft()for _ in range(col)], sep="")
+    else:
+        print(*[snake_copy.popleft() for _ in range(col)][::-1],sep="")
