3b05921c95e65016890df639529469ddec6a90c5
Denitsapeeva <denitsa.peeva@yahoo.com>
2023-02-11 17:43:15 +0200

    Studying programming

--- Exam-Preparation-1/climb_the_peaks.py
+++ Exam-Preparation-1/climb_the_peaks.py
@@ -1,9 +1,10 @@
 from collections import deque
 
-food = deque(x for x in input().split(", "))
-stamina = deque(x for x in input().split(", "))
+food = deque(int(x) for x in input().split(", "))
+stamina = deque(int(x) for x in input().split(", "))
 
 climed_peaks = []
+stop_the_program = False
 
 all_peaks = {"Vihren": 80,
              "Kutelo": 90,
@@ -17,8 +18,19 @@ for key,value in all_peaks.items():
         my_sum = food.pop() + stamina.popleft()
         if my_sum >= value:
             climed_peaks.append(key)
-        elif len(food) == 0 and len(stamina) == 0 and len(climed_peaks) == 0:
+            break
+        if len(food) == 0 and len(stamina) == 0 and len(climed_peaks) < 5:
             print("Alex failed! He has to organize his journey better next time -> @PIRINWINS")
+            stop_the_program = True
+            break
+    if stop_the_program:
+        break
+if len(climed_peaks) == 5:
+    print("Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK")
+if climed_peaks:
+    print("Conquered peaks:\n" + '\n'.join(climed_peaks))
+
+
 
 
 
