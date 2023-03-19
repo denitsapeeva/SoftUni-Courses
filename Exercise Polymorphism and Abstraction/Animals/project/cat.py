196083d58806de6446586a75e613c826e3407b64
Denitsapeeva <denitsa.peeva@yahoo.com>
2023-03-19 18:12:42 +0200

    Studying programming

---
+++ Exercise Polymorphism and Abstraction/Animals/project/cat.py
@@ -0,0 +1,12 @@
+from abc import ABC
+
+from project.animal import Animal
+
+
+class Cat(Animal,ABC):
+    def __repr__(self):
+        return f"This is {self.name}. {self.name} is a {self.age} year old {self.gender} {self.__class__.__name__}"
+
+    @staticmethod
+    def make_sound():
+        return "Meow meow!"
