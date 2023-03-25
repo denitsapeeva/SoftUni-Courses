a54765a39b57735b08ed5ffebe4c87eb8c6517e8
Denitsapeeva <denitsa.peeva@yahoo.com>
2023-03-25 14:57:10 +0200

    Studying programming

---
+++ Exercise Decorators/logged.py
@@ -0,0 +1,12 @@
+def logged(func):
+    def wrapper(*args):
+
+        return f"you called {func.__name__}({', '.join(str(arg) for arg in args)})\nit returned {func(*args)}"
+
+    return wrapper
+
+
+@logged
+def func(*args):
+    return 3 + len(args)
+print(func(4, 4, 4))
